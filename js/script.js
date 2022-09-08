cf21ac22fbd9a8006aa7b66d818d9809342efbdf
Shermatov Davron Devloper full stack <93197110+DavronShermatov@users.noreply.github.com>
2022-09-08 10:43:24 +0300

    massiv haqida

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Massivlar haqida
+const arr=[1, 2, 3, 4,8, 5, 6, 7, 88];
+// arr.push(8); massiv oxiriga element qo'shadi
+// arr.pop(); Massiv oxiridagi elementni olib tashlaydi
+// console.log(arr.length);
+
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// for(let value of arr){
+//     console.log(value);
+// }
+
+// arr.forEach(function(item, index, array){
+//     console.log(`${index}:${item} massiv ${array}`);
+// });
+
+// const movies = prompt("Ko'rgan kinolaringiz","");
+// const usermovies= movies.split(". ");
+
+// console.log(usermovies.join("- "));
+arr.sort();
+console.log(arr);
